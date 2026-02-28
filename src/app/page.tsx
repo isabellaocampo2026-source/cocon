1667188d3461662f45aae31a54ad96142e7da2fe
marvs2026-lgtm <tu-email@ejemplo.com>
2026-02-27 20:07:12 -0500

    Design: Delete PremiumBreak image section as requested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { TopBar } from '@/components/top-bar'
 import { Hero } from '@/components/hero'
-import { PremiumBreak } from '@/components/premium-break'
 import { Problem } from '@/components/problem'
 import { Solution } from '@/components/solution'
 import { Comparison } from '@/components/comparison'
@@ -113,7 +112,6 @@ export default function Home() {
       <TopBar />
       <main>
         <Hero />
-        <PremiumBreak />
         <Problem />
         <Solution />
         <Comparison />
